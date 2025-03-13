f67e181419522ce1e3927853566ebf4438ce205e
Vaclav Bubenicek <vaclav.bubenicek@gmail.com>
2025-03-13 13:36:15 +0100

    Zkouseni vytvoreni .R soubor ze .Rmd souboru a zpet

---
+++ webstrtest.R
@@ -0,0 +1,11 @@
+#'---
+#'title: "Web stranka na zkousku"
+#'author: "Vasek"
+#'date: "2025-03-10"
+#'output: github_document
+#'---
+
+#+ r setup, include = TRUE
+x <- "Zkouska"; y <- "mikrofonu";
+z <- paste(x, y);
+print(z)
