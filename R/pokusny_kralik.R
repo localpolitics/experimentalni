5fb14cdc3024b47df075bd7e85f9543af1c0d3c4
Vaclav Bubenicek <vaclav.bubenicek@gmail.com>
2025-03-14 16:57:41 +0100

    Pokus, jak editovat .R pro zobrazeni jako .md - ted s obrazky

--- R/pokusny_kralik.R
+++ R/pokusny_kralik.R
@@ -6,11 +6,16 @@
 #' Here's some prose in a very special comment. Let's summarize the built-in
 #' dataset `VADeaths`.
 # here is a regular code comment, that will remain as such
+#+ summary, collapse = TRUE
 summary(VADeaths)
 
 #' Here's some more prose. I can use usual markdown syntax to make things
 #' **bold** or *italics*. Let's use an example from the `dotchart()` help to
 #' make a Cleveland dot plot from the `VADeaths` data. I even bother to name
 #' this chunk, so the resulting PNG has a decent name.
-#+ dotchart
+#+ dotchart, fig.show = "hide"
 dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
+
+#' Obrázek s nastavenou šířkou a zarovnáním
+#+ show_image, echo=FALSE, fig.align="center"
+knitr::include_graphics("pokusny_kralik_files/figure-gfm/obr1.jpg")
