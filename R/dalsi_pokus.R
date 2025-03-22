ad9ab01ec064e861afd3a1ddccb2a57c63e96ab7
Vaclav Bubenicek <vaclav.bubenicek@gmail.com>
2025-03-22 21:14:05 +0100

    Funkce pozdrav() pridana.

---
+++ R/dalsi_pokus.R
@@ -0,0 +1,23 @@
+#' ---
+#' title:   "Napis 'ahoj'"
+#' author:  "Vasko"
+#' date:    "`r format(Sys.Date())`"
+#' output:  github_document
+#' ---
+#'
+#' Pozdrav
+#'
+#' @author Vasko
+#'
+#' @description
+#' Napise jen "ahoj"
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' pozdrav()
+#'
+pozdrav <- function(){
+  print("ahoj")
+}
